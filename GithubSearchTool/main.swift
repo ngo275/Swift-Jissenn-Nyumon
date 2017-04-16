9446c6850354c8006c5d60830e665301906b176d
Shuichi Nagao <ngo38114@gmail.com>
2017-04-17 08:53:57 +0900

    Initial Commit

---
+++ GithubSearchTool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GithubSearchTool
+//
+//  Created by ShuichiNagao on 2017/04/17.
+//  Copyright © 2017 ShuichiNagao. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
